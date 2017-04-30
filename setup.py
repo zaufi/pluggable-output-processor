246f9fed3d6388cdf982b9fda915bec7c71e8a49
Alex Turbov <i.zaufi@gmail.com>
2017-04-30 21:28:40 +0700

    Fix python version classifier

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
       , 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
       , 'Natural Language :: English'
       , 'Operating System :: POSIX :: Linux'
-      , 'Programming Language :: Python :: 3 :: 3.5'
+      , 'Programming Language :: Python :: 3.5'
       , 'Topic :: Utilities'
       ]
   , keywords = 'console gcc make cmake ouput colorizer'
