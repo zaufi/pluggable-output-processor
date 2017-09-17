ddbacbfd749dbedcd8134735e11d2d31e8a1ed8f
Alex Turbov <i.zaufi@gmail.com>
2017-09-17 14:00:04 +0200

    Recognize more `not-founds` of CMake output

--- outproc/pp/cmake.py
+++ outproc/pp/cmake.py
@@ -28,7 +28,7 @@ import shlex
 
 _SUCCESS_RE = re.compile('^-- (Check|Looking|Performing Test|Detecting).*-{1,2} (works|done|yes|found|Success)$')
 _SUCCESS2_RE = re.compile('^-- Found .*:\s.*$')
-_FAILURE_RE = re.compile('^-- .* - (not found|Failed)$')
+_FAILURE_RE = re.compile('^-- .* - (no|not found|Failed)$')
 _FATAL_RE = re.compile('^CMake Error.*')
 
 
